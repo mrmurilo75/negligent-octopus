d0db26438cca89c6c2a49c0d2877d3393bfbfa65
Murilo Rosa <mrmurilo75@gmail.com>
2024-02-28 01:29:27 +0000

    initialize from cookiecutter django

---
+++ negligent_octopus/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from negligent_octopus.users.models import User
+from negligent_octopus.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
