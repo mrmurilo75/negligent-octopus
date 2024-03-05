100a1fbcde914126c5234f22830ef0b6ddeba3c5
Murilo Rosa <mrmurilo75@gmail.com>
2024-03-05 12:49:24 +0000

    create base models

--- negligent_octopus/core/admin.py
+++ negligent_octopus/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account
+from .models import Transaction
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ["name", "owner", "balance", "modified", "created", "is_removed"]
+    search_fields = ["name", "owner__username"]
+    list_filter = ["is_removed", "modified"]
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["title", "account", "get_account_owner", "date", "amount"]
+    search_fields = ["account__owner__username", "account__name", "title"]
+    list_filter = ["account__owner", "account", "date"]
--- negligent_octopus/core/models.py
+++ negligent_octopus/core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from model_utils.models import SoftDeletableModel
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+from negligent_octopus.users.models import User
+
+
+class Account(TimeStampedModel, SoftDeletableModel):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    balance = models.FloatField(default=0.0, editable=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Account"
+        verbose_name_plural = "Accounts"
+        ordering = ["-modified", "name"]
+
+
+class Transaction(TimeStampedModel):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    date = models.DateField(default=timezone.now)
+    title = models.CharField(max_length=127)
+    description = models.TextField(blank=True)
+
+    def get_account_owner(self):
+        return str(self.account.owner)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Transaction"
+        verbose_name_plural = "Transactions"
+        ordering = ["date", "-modified"]
