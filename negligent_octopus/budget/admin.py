424ab73731990afdde1f7cd85fcfae09cee94a5f
Murilo Rosa <mrmurilo75@gmail.com>
2024-04-18 12:58:10 +0100

    reimplement imported activo transaction model and admin

--- negligent_octopus/budget/admin.py
+++ negligent_octopus/budget/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 
+from negligent_octopus.utils.admin import LimitedQuerysetInlineAdmin
+from negligent_octopus.utils.admin import LimitedQuerysetInlineFormset
+
 from .models import ImportActivo
+from .models import ImportedActivoTransaction
+
+
+class ImportedActivoTransactionInlineFormset(LimitedQuerysetInlineFormset):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs)[::-1]
+
+
+class ImportedActivoTransactionInlineAdmin(LimitedQuerysetInlineAdmin):
+    model = ImportedActivoTransaction
+    fk_name = "loaded_from"
+    fields = [
+        "loaded_from",
+        "date_of_movement",
+        "date_of_process",
+        "description",
+        "value",
+        "balance",
+        "validated",
+        "transaction",
+    ]
+    extra = 0
+    formset = ImportedActivoTransactionInlineFormset
 
 
 @admin.register(ImportActivo)
@@ -14,6 +40,7 @@ class ImportActivoAdmin(admin.ModelAdmin):
         "processed",
         "created",
     ]
+    inlines = [ImportedActivoTransactionInlineAdmin]
 
     def get_readonly_fields(
         self,
@@ -26,3 +53,54 @@ class ImportActivoAdmin(admin.ModelAdmin):
         if obj.processed:
             readonly_fields += ["account", "processed"]
         return readonly_fields
+
+
+@admin.register(ImportedActivoTransaction)
+class ImportedActivoTransactionAdmin(admin.ModelAdmin):
+    fields = [
+        "loaded_from",
+        "date_of_movement",
+        "date_of_process",
+        "description",
+        "value",
+        "balance",
+        "validated",
+        "transaction",
+    ]
+    list_display = [
+        "get_load_owner",
+        "date_of_movement",
+        "date_of_process",
+        "description",
+        "value",
+        "balance",
+        "validated",
+        "has_transaction",
+    ]
+    list_display_links = ["description"]
+    search_fields = [
+        "description",
+        "value",
+        "balance",
+    ]
+    list_filter = [
+        "loaded_from__owner",
+        "date_of_movement",
+        "date_of_process",
+        "validated",
+    ]
+    readonly_fields = ["loaded_from"]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj is not None and obj.validated and obj.transaction is not None:
+            return [
+                *self.readonly_fields,
+                "date_of_movement",
+                "date_of_process",
+                "description",
+                "value",
+                "balance",
+                "validated",
+                "transaction",
+            ]
+        return self.readonly_fields
--- negligent_octopus/budget/models.py
+++ negligent_octopus/budget/models.py
@@ -3,6 +3,7 @@ from django.utils.text import slugify
 from model_utils.models import TimeStampedModel
 
 from negligent_octopus.core.models import Account
+from negligent_octopus.core.models import Transaction
 from negligent_octopus.users.models import User
 from negligent_octopus.utils import get_filename_extension
 from negligent_octopus.utils import get_filename_no_extension
@@ -19,7 +20,18 @@ def upload_activo_import_to(instance, filename):
 
 
 class ImportActivo(TimeStampedModel):
-    ALLOWED_EXTENSIONS = ("csv", "xsls")
+    # TODO deal with each when importing - for activo we only deal with excel
+    ALLOWED_EXTENSIONS = (
+        "csv",
+        "xsls",
+        "xls",
+        "xlsx",
+        "xlsm",
+        "xlsb",
+        "odf",
+        "ods",
+        "odt",
+    )
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=True)
@@ -35,16 +47,49 @@ class ImportActivo(TimeStampedModel):
     )
     processed = models.BooleanField(default=False)
 
+    def _set_name_from_filename(self):
+        try:
+            name = self.load.name.rsplit("/", 1)[1]
+        except IndexError:
+            name = self.load.name
+        self.name = get_filename_no_extension(name)
+
     def save(self, *args, **kwargs):
         if not self.name:
-            try:
-                name = self.load.name.rsplit("/", 1)[1]
-            except IndexError:
-                name = self.load.name
-            self.name = get_filename_no_extension(name)
+            self._set_name_from_filename()
         super().save(*args, **kwargs)
 
     class Meta(TimeStampedModel.Meta):
         verbose_name = "Activo Import"
         verbose_name_plural = "Activo Imports"
         ordering = ["created"]
+
+
+class ImportedActivoTransaction(TimeStampedModel):
+    loaded_from = models.ForeignKey(ImportActivo, on_delete=models.RESTRICT)
+    date_of_movement = models.DateField()
+    date_of_process = models.DateField()
+    description = models.CharField(max_length=255, blank=True)
+    value = models.FloatField()
+    balance = models.FloatField()
+    validated = models.BooleanField(default=False)
+    transaction = models.OneToOneField(
+        Transaction,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    def get_load_owner(self):
+        return str(self.loaded_from.owner)
+
+    def has_transaction(self):
+        return self.transaction is not None
+
+    def __str__(self):
+        return str(self.description)
+
+    class Meta(TimeStampedModel.Meta):
+        verbose_name = "Imported Activo Transaction"
+        verbose_name_plural = "Imported Activo Transactions"
+        ordering = ["-date_of_process", "-created"]
