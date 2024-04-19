fe75275d09f66a58e2114b65842f0a436b548fd3
Murilo Rosa <mrmurilo75@gmail.com>
2024-04-19 14:36:29 +0100

    open excel to import transaction - in progress

--- negligent_octopus/budget/management/commands/load_activo.py
+++ negligent_octopus/budget/management/commands/load_activo.py
@@ -1,4 +1,24 @@
+from pathlib import Path
+
+import pandas as pd
+from django.conf import settings
 from django.core.management.base import BaseCommand
+
+from negligent_octopus.budget.models import ImportActivo
+
+last = ImportActivo.objects.last().load
+with (Path(settings.MEDIA_ROOT) / Path(last.name)).open("rb") as xslx:
+    pd.read_excel(
+        xslx,
+        skiprows=7,
+        names=[
+            "date_of_movement",
+            "date_of_process",
+            "description",
+            "value",
+            "balance",
+        ],
+    )
 
 
 class Command(BaseCommand):
