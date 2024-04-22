ab4dd22c4cb7f63a6784959ca60ee7961f1df141
Murilo Rosa <mrmurilo75@gmail.com>
2024-04-22 20:52:55 +0100

    refactor and implement tests - in progress

--- negligent_octopus/core/tests/test_models.py
+++ negligent_octopus/core/tests/test_models.py
@@ -15,6 +15,173 @@ faker = Faker()
 
 
 @pytest.mark.django_db()
+class TestAccount:
+    def test_create(self):
+        initial = 1.1
+        acc = AccountFactory(
+            name="Test",
+            initial_balance=initial,
+        )
+        assert acc.name == "Test"
+        assert acc.initial_balance == initial
+        assert acc.balance == initial
+
+    def test_add_transactions(self, account: Account):
+        balance = account.initial_balance
+        for i, value in enumerate(range(5), 1):
+            balance += value
+            TransactionFactory(
+                account=account,
+                amount=value,
+                timestamp=timezone.now(),  # Make sure they are in order
+            )
+            assert account.balance == balance
+            assert account.transaction_set.count() == i
+
+    def test_change_initial_balance(self, account: Account):
+        for value in range(5):
+            TransactionFactory(
+                account=account,
+                amount=value,
+                timestamp=timezone.now(),  # Make sure they are in order
+            )
+        old_balance = account.balance
+        account.initial_balance += 1.1
+        account.save()
+
+        account = Account.objects.get(pk=account.pk)
+
+        assert account.balance == old_balance + 1.1
+
+    def test_delete(self, account: Account):
+        account.delete()
+
+        with pytest.raises(Account.DoesNotExist):
+            Account.objects.get(pk=account.pk)
+
+
+@pytest.mark.django_db()
+class TestTransaction:
+    def test_create(self, account: Account):
+        value = 1.1
+        transaction = TransactionFactory(
+            account=account,
+            amount=value,
+        )
+        assert transaction.balance == account.initial_balance + value
+        assert not transaction.is_transfer
+
+    def test_sequence(self, account: Account):
+        def inner_test_sequence(first, middle, last):
+            first = Transaction.objects.get(pk=first.pk)
+            middle = Transaction.objects.get(pk=middle.pk)
+            last = Transaction.objects.get(pk=last.pk)
+
+            assert list(account.transaction_set.all()) == [
+                last,
+                middle,
+                first,
+            ]
+
+            assert first.next() == middle
+            assert middle.previous() == first
+
+            assert list(first.after()) == [
+                last,
+                middle,
+            ]
+            assert list(last.before()) == [
+                middle,
+                first,
+            ]
+
+            assert first.previous() is None
+            assert last.next() is None
+
+            assert len(first.before()) == 0
+            assert len(last.after()) == 0
+
+        transactions = [
+            TransactionFactory(
+                account=account,
+                title=title,
+                timestamp=timezone.now(),  # Make sure they are in order
+            )
+            for title in ("first", "middle", "last")
+        ]
+        inner_test_sequence(*transactions)
+
+        # Move middle backward by timestamp
+        transactions[1].title = "new_first"
+        transactions[1].timestamp = transactions[0].timestamp - timedelta(seconds=1)
+        transactions[1].save()
+        transactions[0], transactions[1] = transactions[1], transactions[0]
+        inner_test_sequence(*transactions)
+
+        # Move first forward by timestamp
+        transactions[0].title = "new_middle"
+        transactions[0].timestamp = transactions[2].timestamp
+        # Same timestamp but created earlier
+        transactions[0].save()
+        transactions[0], transactions[1] = transactions[1], transactions[0]
+        inner_test_sequence(*transactions)
+
+        # TODO Insert transaction between 1 and 2
+
+        # TODO Delete a transaction
+
+    def test_balance(self, account: Account):
+        def inner_test_balance(first, middle, last):
+            first = Transaction.objects.get(pk=first.pk)
+            middle = Transaction.objects.get(pk=middle.pk)
+            last = Transaction.objects.get(pk=last.pk)
+
+            assert first.balance == account.initial_balance + first.amount
+            assert middle.balance == first.balance + middle.amount
+            assert last.balance == middle.balance + last.amount
+
+        transactions = []
+        values = [(value + value / 10) for value in range(3)]
+        for title, value in enumerate(values):
+            transactions.append(
+                TransactionFactory(
+                    account=account,
+                    title=title,
+                    amount=value,
+                    timestamp=timezone.now(),  # Make sure they are in order
+                ),
+            )
+        inner_test_balance(*transactions)
+
+        # Move middle backward by timestamp
+        transactions[1].title = "new_0"
+        transactions[1].timestamp = transactions[0].timestamp - timedelta(seconds=1)
+        transactions[1].save()
+        transactions[0], transactions[1] = transactions[1], transactions[0]
+        inner_test_balance(*transactions)
+
+        # Move first forward by timestamp
+        transactions[0].title = "new_1"
+        transactions[0].timestamp = transactions[2].timestamp
+        # Same timestamp but created earlier
+        transactions[0].save()
+        transactions[0], transactions[1] = transactions[1], transactions[0]
+        inner_test_balance(*transactions)
+
+        # Change amount
+        transactions[0].amount += 1.1
+        transactions[0].save()
+        inner_test_balance(*transactions)
+
+        # TODO Insert transaction between 1 and 2
+
+        # TODO Delete a transaction
+
+    def test_transfer(self, account: Account):
+        raise NotImplementedError
+
+
+@pytest.mark.django_db()
 def test_transaction_transfer(account: Account):
     transaction = TransactionFactory(
         account=account,
@@ -67,17 +234,6 @@ def test_transaction_transfer_delete(account: Account):
 
 @pytest.mark.django_db()
 class TestAccountTransactionBalance:
-    def test_account_balance(self, account: Account):
-        balance = account.initial_balance
-        for i in range(10):
-            balance += i
-            TransactionFactory(
-                account=account,
-                amount=i,
-                timestamp=timezone.now(),  # Make sure they are in order
-            )
-        assert account.balance == balance
-
     def test_transaction_balance(self, account: Account):
         balance = account.initial_balance
         for i in range(10):
