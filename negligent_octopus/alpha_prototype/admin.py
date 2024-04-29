c76ba0e4fb69ab13f294e9209df71420d4ed88bb
Murilo Rosa <mrmurilo75@gmail.com>
2024-04-29 22:41:37 +0100

    define and register new admin model - in progress

--- negligent_octopus/alpha_prototype/admin.py
+++ negligent_octopus/alpha_prototype/admin.py
@@ -1,5 +1,16 @@
 from django.contrib.admin.sites import AdminSite
 
+from negligent_octopus.budget.admin import ImportActivoAdmin  # noqa: F401
+from negligent_octopus.budget.admin import ImportedActivoTransactionAdmin  # noqa: F401
+from negligent_octopus.budget.models import ImportActivo  # noqa: F401
+from negligent_octopus.budget.models import ImportedActivoTransaction  # noqa: F401
+from negligent_octopus.core.admin import AccountAdmin
+from negligent_octopus.core.admin import CategoryAdmin  # noqa: F401
+from negligent_octopus.core.admin import TransactionAdmin  # noqa: F401
+from negligent_octopus.core.models import Account
+from negligent_octopus.core.models import Category  # noqa: F401
+from negligent_octopus.core.models import Transaction  # noqa: F401
+
 
 class UserAdmin(AdminSite):
     # TODO Anything we wish to add or override
@@ -9,3 +20,12 @@ class UserAdmin(AdminSite):
 
 
 alpha_admin_site = UserAdmin(name="alpha_prototype")
+
+
+class OpenAccountAdmin(AccountAdmin):
+    # TODO Define permissions and filter by request.user
+    pass
+
+
+alpha_admin_site.register(Account, OpenAccountAdmin)
+# TODO Apply same pattern for other classes (removed noqa once done)
